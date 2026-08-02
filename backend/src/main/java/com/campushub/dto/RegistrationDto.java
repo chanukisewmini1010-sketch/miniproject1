66d121fd3d639bdca02ca9c19167675483e9d1c0
chanuki sewmini <chanukisewmini1010@gmail.com>
2026-08-02 11:48:29 +0530

    Add RegistrationDto for registration data transfe

--- backend/src/main/java/com/campushub/dto/RegistrationDto.java
+++ backend/src/main/java/com/campushub/dto/RegistrationDto.java
@@ -4,10 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Data Transfer Object for Registration.
- * TODO (Member 5): map fields to/from the Registration entity in the service layer.
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
